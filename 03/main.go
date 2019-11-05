5d1595012217f05b48b2d2f476337d759c48d6a4
x86nick <narender_kumar@intuit.com>
2019-11-04 21:26:06 -0800

    declare var

---
+++ 03/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var i int
+	// var s string
+
+	// i = 10
+	// s = "hello world"
+	var i int = 10
+	var s string = "hello world"
+	fmt.Println(i)
+	fmt.Println(s)
+}
