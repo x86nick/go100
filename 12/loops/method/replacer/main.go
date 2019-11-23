dec4b90557c26f97c9f7a053b35242603d1c1305
x86nick <narender_kumar@intuit.com>
2019-11-23 11:14:30 -0800

    replacer

--- 12/loops/method/replacer/main.go
+++ 12/loops/method/replacer/main.go
@@ -5,12 +5,15 @@ import (
 	"strings"
 )
 
+// broken := "Hell# W#rld" // syntax error: non-declaration statement outside function body
 func main() {
-	// broken := "Hell# W#rld"
-	var broken string = "Hell# W#rld"
-	r := strings.NewReplacer("#", "o")
+	broken := "Hell# W#rld"
+	// var broken string = "Hell# W#rld"
+	r := strings.NewReplacer("#", "o") // Returns strings.Replacer thats setup to replace every # with o.
+	fmt.Println(r)                     // &{{{0 0} 0} <nil> [# o]}
 	// replacer := strings.NewReplacer("#", "o")
+	fmt.Println(broken) // Hell# W#rld
+	// var fixed string = r.Replace(broken)
 	fixed := r.Replace(broken)
-	fmt.Println(fixed)
-
+	fmt.Println(fixed) // Hello World
 }
