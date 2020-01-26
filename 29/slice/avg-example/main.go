b297be948b7f03d2fdddcf349146f41847798092
x86nick <narender_kumar@intuit.com>
2020-01-26 15:08:30 -0800

    get avg

---
+++ 29/slice/avg-example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/x86nick/datafile"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
